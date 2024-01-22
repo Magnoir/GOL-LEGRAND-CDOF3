5d5f2f0b38ce95e2cf4a7dbc65d0954be2460471
mathis <mathious61@gmail.com>
2024-01-22 19:31:23 +0100

    Feature: clear the console before every changes. Resolves #3

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ function nextGeneration(board) {
 }
 
 function printBoard(board) {
+    console.clear(); // clear console to avoid scrolling
     for (let i = 0; i < boardSize; i++) {
         let row = '';
         for (let j = 0; j < boardSize; j++) {
